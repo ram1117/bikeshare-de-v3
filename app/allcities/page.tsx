9a95d2892c9a0b9acabd5e1c1f46b3d01affbdb6
ram1117 <ramkumark1@outlook.com>
2023-12-05 01:33:21 +0530

    trying to fix build error

--- app/allcities/page.tsx
+++ app/allcities/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { refineBikeData } from "@/utils";
 import AllCitiesWrapper from "@/components/allcities/AllCitiesWrapper";
 
-export const fetchBikeData = async () => {
+export const fetchBikeData1 = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BIKEDATA_API_URL}`);
 
   if (!res.ok) {
@@ -14,7 +14,7 @@ export const fetchBikeData = async () => {
 };
 
 const CitiesPage = async () => {
-  const refinedData = await fetchBikeData();
+  const refinedData = await fetchBikeData1();
   return (
     <section className="flex flex-col items-center justify-start min-h-[90vh] h-max">
       <AllCitiesWrapper data={refinedData} />
