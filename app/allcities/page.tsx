6979a65cdc7a47d79a294d7ae173f2d0c6088015
ram1117 <ramkumark1@outlook.com>
2023-12-05 01:38:27 +0530

    reusing fetchbike data in allcities page

--- app/allcities/page.tsx
+++ app/allcities/page.tsx
@@ -1,20 +1,9 @@
 import React from "react";
-import { refineBikeData } from "@/utils";
 import AllCitiesWrapper from "@/components/allcities/AllCitiesWrapper";
-
-export const fetchBikeData1 = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BIKEDATA_API_URL}`);
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  const bikeNetworkData = await res.json();
-  const refinedData = refineBikeData(bikeNetworkData);
-  return refinedData;
-};
+import { fetchBikeData } from "../page";
 
 const CitiesPage = async () => {
-  const refinedData = await fetchBikeData1();
+  const refinedData = await fetchBikeData();
   return (
     <section className="flex flex-col items-center justify-start min-h-[90vh] h-max">
       <AllCitiesWrapper data={refinedData} />
